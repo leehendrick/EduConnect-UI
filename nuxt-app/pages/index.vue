c08696e58677601d2ce209006b8574cd0a775378
leehendrick <imhendrick16@gmail.com>
2024-04-18 22:25:31 +0100

    add landing page main content

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,7 +1,45 @@
 <template>
-  <div>
-    <h1 class="font-bold text-9xl">Hello, Nuxt</h1>
-  </div>
+  <main class="isolate">
+    <!-- Hero section -->
+    <div class="relative pt-14">
+      <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+        <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+      </div>
+      <div class="relative w-full h-screen">
+        <img class="w-full h-full object-cover" src="/estudante-sem-fundo.png" alt="Imagem">
+        <div class="absolute inset-0 flex justify-center items-center">
+          <div class="bg-opacity-50 p-6  w-full h-full object-cover">
+            <div class="py-24 sm:py-32">
+              <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                <div class="mx-auto max-w-2xl text-center lg:text-left lg:ml-0 overflow-hidden">
+                  <h1 class="typed-text  font-bold tracking-tight text-primary sm:text-6xl">Surpreenda-se </h1>
+                  <h1 class="typed-text  font-bold tracking-tight text-primary sm:text-6xl">com o aprendizado no</h1>
+                  <h1 class="typed-text  font-bold tracking-tight text-primary sm:text-6xl">IMPTEL</h1>
+
+                  <p class="mt-6 text-lg leading-8 text-black">Explore um mundo de aprendizado <strong> emocionante e surpreendente no IMPTEL</strong>, onde cada descoberta é uma jornada de alegria e crescimento!</p>
+                  <div class="mt-10 flex items-center justify-center gap-x-6">
+                    <a href="#" class="rounded-md bg-primary transition duration-300 ease-in-out px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+                    <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="w-full h-full relative overflow-hidden">
+        <img src="/tela2.svg" alt="">
+
+      </div>
+
+      <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+        <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+      </div>
+    </div>
+
+  </main>
 </template>
 <script setup lang="ts">
+import Landing from "~/layouts/default.vue";
 </script>
