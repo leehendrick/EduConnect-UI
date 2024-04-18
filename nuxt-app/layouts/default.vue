994d2449468ba89b92a4806f465b46004d8802f5
leehendrick <imhendrick16@gmail.com>
2024-04-18 22:24:15 +0100

    add the landing page layout

---
+++ nuxt-app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-full h-screen">
+    <Header/>
+      <slot/>
+    <Footer/>
+  </div>
+</template>
+<script setup lang="ts">
+import Header from "~/pages/components/header.vue";
+import Footer from "~/pages/components/footer.vue";
+</script>
