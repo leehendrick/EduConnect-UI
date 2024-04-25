eeec792ed3727371faed2626d42e4f08213280ae
leehendrick <imhendrick16@gmail.com>
2024-04-25 22:29:10 +0100

    add : border bottom to the nav menu

--- nuxt-app/pages/components/header.vue
+++ nuxt-app/pages/components/header.vue
@@ -13,7 +13,7 @@ function toggle(status: number){
 
 <template>
   <header class="fixed inset-x-0 top-0 z-50 bg-white">
-    <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <nav class="flex items-center justify-between p-6 lg:px-8 border-b border-primary" aria-label="Global">
       <div class="flex lg:flex-1">
         <a href="#" class="-m-1.5 p-1.5">
           <span class="sr-only">Your Company</span>
