fae7dd6333be76451977e06fd9b7ebd12e5e2db0
leehendrick <imhendrick16@gmail.com>
2024-04-30 08:06:25 +0100

    remove gray background

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -86,7 +86,7 @@
         <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
       </div>
 
-      <div class="bg-gray-100 py-8 md:py-12 lg:py-16">
+      <div class="py-8 md:py-12 lg:py-16">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div class="text-center">
             <h2 class="text-3xl font-extrabold sm:text-4xl lg:text-5xl text-primary">Notas Informativas</h2>
@@ -126,7 +126,7 @@
         </div>
       </div>
 
-      <div class="bg-gray-100 py-8 md:py-12 lg:py-16">
+      <div class="py-8 md:py-12 lg:py-16">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div class="text-center border-t border-primary">
             <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl lg:text-5xl mt-10">Quadro de Honra</h2>
