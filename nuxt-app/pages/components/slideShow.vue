c3c36c44ba7494d74c7b07104b8738e7181ebf48
leehendrick <imhendrick16@gmail.com>
2024-05-23 11:34:34 +0100

    add slideshow component

---
+++ nuxt-app/pages/components/slideShow.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="relative w-full max-w-4xl mx-auto">
+    <!-- Slides -->
+    <div v-for="(slide, index) in slides" :key="index" class="slides" :class="{ 'block': index === currentSlide, 'hidden': index !== currentSlide }">
+      <img :src="slide" alt="Slide Image" class="w-full h-auto rounded-lg">
+    </div>
+
+    <!-- Navigation Buttons -->
+    <button class="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-800 text-white px-3 py-2 rounded-full hover:bg-gray-900" @click="prevSlide">&#10094;</button>
+    <button class="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-800 text-white px-3 py-2 rounded-full hover:bg-gray-900" @click="nextSlide">&#10095;</button>
+
+    <!-- Indicators -->
+    <div class="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+      <span v-for="(slide, index) in slides" :key="'indicator-' + index" @click="currentSlide = index" :class="indicatorClass(index)"></span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const slides = ref([
+  '/estudante-sem-fundo.png',
+  '/estudante-black-white.png',
+  '/estudante-sem-fundo.png',
+]);
+
+const currentSlide = ref(0);
+let slideInterval;
+
+const prevSlide = () => {
+  currentSlide.value = (currentSlide.value > 0) ? currentSlide.value - 1 : slides.value.length - 1;
+};
+
+const nextSlide = () => {
+  currentSlide.value = (currentSlide.value < slides.value.length - 1) ? currentSlide.value + 1 : 0;
+};
+
+const startSlideShow = () => {
+  slideInterval = setInterval(nextSlide, 4000); // Mudar slide a cada 3 segundos
+};
+
+const stopSlideShow = () => {
+  clearInterval(slideInterval);
+};
+
+const indicatorClass = (index) => {
+  return currentSlide.value === index ? 'bg-white w-3 h-3 rounded-full cursor-pointer' : 'bg-gray-500 w-3 h-3 rounded-full cursor-pointer';
+};
+
+onMounted(() => {
+  startSlideShow();
+});
+
+onUnmounted(() => {
+  stopSlideShow();
+});
+</script>
+
+<style scoped>
+.slides {
+  transition: opacity 0.5s ease-in-out;
+}
+</style>
