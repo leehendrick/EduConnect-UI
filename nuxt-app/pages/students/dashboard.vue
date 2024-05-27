94b07804488ac5f40c40f3072453bc313625bb13
leehendrick <imhendrick16@gmail.com>
2024-05-27 21:44:11 +0100

    add dashboard template component

---
+++ nuxt-app/pages/students/dashboard.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+definePageMeta({
+  layout: 'students-layout',
+})
+</script>
+
+<template>
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900">You're logged in as student!</div>
+      </div>
+    </div>
+  </div>
+</template>
