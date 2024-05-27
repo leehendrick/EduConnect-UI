b87622ff89296d3127901af6be7fcaf367ee32d9
leehendrick <imhendrick16@gmail.com>
2024-05-27 22:42:01 +0100

    add stats section

--- nuxt-app/pages/students/dashboard.vue
+++ nuxt-app/pages/students/dashboard.vue
@@ -3,14 +3,24 @@
 definePageMeta({
   layout: 'students-layout',
 })
+
+const stats = [
+  { name: 'Total Subscribers', stat: '71,897' },
+  { name: 'Avg. Open Rate', stat: '58.16%' },
+  { name: 'Avg. Click Rate', stat: '24.57%' },
+]
 </script>
 
 <template>
-  <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 text-gray-900">You're logged in as student!</div>
-      </div>
+  <div class="">
+    <div>
+      <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+
+        <div v-for="item in stats" :key="item.name" class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+          <dt class="truncate text-sm font-medium text-gray-500">{{ item.name }}</dt>
+          <dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{{ item.stat }}</dd>
+        </div>
+      </dl>
     </div>
   </div>
 </template>
