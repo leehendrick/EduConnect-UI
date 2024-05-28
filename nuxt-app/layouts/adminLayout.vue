fdfd3a490dea9352d8ad131598b760b985ff8f3a
leehendrick <imhendrick16@gmail.com>
2024-05-28 10:10:41 +0100

    add project name

--- nuxt-app/layouts/adminLayout.vue
+++ nuxt-app/layouts/adminLayout.vue
@@ -67,6 +67,7 @@
       <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
         <div class="flex h-16 shrink-0 items-center">
           <img class="h-8 w-auto bg-white rounded-2xl" src="/IMPTEL.svg" alt="Imptel Logo" />
+          <h1 class="text-white ml-3 text-xl font-extrabold">IMPTELGest</h1>
         </div>
         <nav class="flex flex-1 flex-col">
           <ul role="list" class="flex flex-1 flex-col gap-y-7">
