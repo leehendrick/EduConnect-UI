44c521db00d723cecade89924b5237366df988e4
leehendrick <imhendrick16@gmail.com>
2024-05-28 10:46:14 +0100

    change div style

--- nuxt-app/pages/students/dashboard.vue
+++ nuxt-app/pages/students/dashboard.vue
@@ -17,7 +17,7 @@ const stats = [
       <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
         <div v-for="item in stats" :key="item.name" class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow-md shadow-secondary sm:p-6">
           <dt class="truncate text-sm font-medium text-gray-500">{{ item.name }}</dt>
-          <dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{{ item.stat }}</dd>
+          <dd class="mt-1 text-3xl font-semibold tracking-tight text-secondary">{{ item.stat }}</dd>
         </div>
       </dl>
     </div>
