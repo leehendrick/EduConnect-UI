7442cf1b086b68a5c17b68012cfe73d5907386dc
leehendrick <imhendrick16@gmail.com>
2024-05-31 09:54:26 +0100

    feature: refactor horario tables

--- nuxt-app/pages/students/horario.vue
+++ nuxt-app/pages/students/horario.vue
@@ -13,23 +13,43 @@ const horario = [
   { tempo: "16H50 - 17H35", ord: "6º", segunda: "TREI", terca: "", quarta: "ING. TEC.", quinta: "FIS", sexta: "TLP" },
 ]
 
+const students = {
+  curso: 'Técnico de Informática',
+  classe: "12ª",
+  turma: "INF-12A",
+  sala: "13"
+}
+
 </script>
 
 <template>
+  <div class="">
+    <div class="bg-secondary overflow-hidden shadow-gray-500 shadow-md sm:rounded-lg">
+      <div class="text-white space-x-2 p-5 flex">
+        <p>CURSO: {{students.curso}}</p>
+        <div class="flex pl-96 space-x-2">
+          <p>CLASSE: {{students.classe}}</p>
+          <p>TURMA: {{students.turma}}</p>
+          <p>SALA: {{students.sala}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
     <div class="px-4 sm:px-6 lg:px-8">
       <div class="mt-8 flow-root">
         <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-            <table class="min-w-full divide-y divide-gray-300">
+            <table class="min-w-full divide-y divide-gray-300 border-2">
               <thead>
-              <tr>
-                <th scope="col" class="px-3 py-3.5 text-left text-md font-extrabold text-gray-900">TEMPO</th>
-                <th scope="col" class="px-3 py-3.5 text-left text-md font-extrabold text-gray-900">ORD</th>
-                <th scope="col" class="px-3 py-3.5 text-left text-md font-extrabold text-gray-900">SEGUNDA</th>
-                <th scope="col" class="px-3 py-3.5 text-left text-md font-extrabold text-gray-900">TERÇA</th>
-                <th scope="col" class="px-3 py-3.5 text-left text-md font-extrabold text-gray-900">QUARTA</th>
-                <th scope="col" class="px-3 py-3.5 text-left text-md font-extrabold text-gray-900">QUINTA</th>
-                <th scope="col" class="px-3 py-3.5 text-left text-md font-extrabold text-gray-900">SEXTA</th>
+              <tr class="bg-secondary  text-white">
+                <th scope="col" class="px-3 py-3.5 text-left text-md font-extrabold ">TEMPO</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-md font-extrabold ">ORD</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-md font-extrabold ">SEGUNDA</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-md font-extrabold ">TERÇA</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-md font-extrabold ">QUARTA</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-md font-extrabold ">QUINTA</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-md font-extrabold ">SEXTA</th>
               </tr>
               </thead>
               <tbody class="divide-y divide-gray-200">
