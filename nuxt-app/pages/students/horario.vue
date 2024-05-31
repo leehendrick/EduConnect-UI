b21c4d04096e7f8e8a994538085a09d76238a975
leehendrick <imhendrick16@gmail.com>
2024-05-31 10:02:53 +0100

    feature: add formadores table

--- nuxt-app/pages/students/horario.vue
+++ nuxt-app/pages/students/horario.vue
@@ -20,6 +20,18 @@ const students = {
   sala: "13"
 }
 
+const formadores = [
+  { disciplina: "Matemática", nome:"Anselmo Lourenço"},
+  { disciplina: "Projecto Tecnologico", nome:"Odete Ferreira"},
+  { disciplina: "Técnicas e Linguagens de Programação", nome:"Odete Ferreira"},
+  { disciplina: "Sistema de Exploração e Arquitectura de Computadores - SEAC", nome:"Manuel Dos Santos"},
+  { disciplina: "Física", nome:"Joelson Damião"},
+  { disciplina: "Organização e Gestão Industrial - OGI", nome:"Cláudia João"},
+  { disciplina: "Técnicas de Reparação de Equipamentos Informáticos - TREI", nome:"Giovani Vita"},
+  { disciplina: "Empreendedorismo", nome:"Márcio De Almeida"},
+  { disciplina: "Inglês Técnico", nome:"Mauro Alexandre"}
+]
+
 </script>
 
 <template>
@@ -68,6 +80,29 @@ const students = {
         </div>
       </div>
     </div>
+
+  <div class="px-4 sm:px-6 lg:px-8">
+    <div class="mt-8 flow-root">
+      <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <table class="min-w-full divide-y divide-gray-300 border-2">
+            <thead>
+            <tr class="bg-secondary  text-white">
+              <th scope="col" class="px-3 py-3.5 text-left text-md font-extrabold ">DISCIPLINA</th>
+              <th scope="col" class="px-3 py-3.5 text-left text-md font-extrabold ">NOME DO PROFESSOR</th>
+            </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200">
+            <tr v-for="item in formadores">
+              <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ item.disciplina }}</td>
+              <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ item.nome }}</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
   </template>
 <style scoped>
 
