2ffb5c20c84a1e46078de281f62246c0d4c90a1e
leehendrick <imhendrick16@gmail.com>
2024-06-03 06:30:18 +0100

    update: remove learn more link

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -71,7 +71,6 @@
                   <p class="mt-6 text-lg leading-8 font-san-francisco text-black">Explore um mundo de aprendizado <strong> emocionante e surpreendente no IMPTEL</strong>, onde cada descoberta é uma jornada de alegria e crescimento!</p>
                   <div class="mt-10 flex items-center justify-center gap-x-6">
                     <a href="#" class="rounded-md bg-primary transition duration-300 ease-in-out px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-                    <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
                   </div>
                 </div>
               </div>
