d6652400ec5c63fa1805373ec1526798c8e3b802
leehendrick <imhendrick16@gmail.com>
2024-06-08 20:21:48 +0100

    feature : add card for notas informativas

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -131,12 +131,24 @@
             <div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
               <!-- Nota Informativa -->
               <div  v-for="item in notasInformativas" class="bg-white overflow-hidden shadow rounded-lg border-2 border-primary">
-                <div class="px-4 py-5 sm:p-6">
-                  <h3 class="text-lg font-medium text-gray-900">{{item.title}}</h3>
-                  <div class="mt-2 text-sm text-gray-600">
-                    <p>{{item.content}}</p>
+                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                  <a href="#">
+                    <img class="" src="/feira_de_emp.jpeg" alt="" />
+                  </a>
+                  <div class="p-5">
+                    <a href="#">
+                      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ item.title }}</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{item.content}}</p>
+                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                      Read more
+                      <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                      </svg>
+                    </a>
                   </div>
                 </div>
+
               </div>
             </div>
           </div>
