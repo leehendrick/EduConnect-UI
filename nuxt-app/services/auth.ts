5cf1d0da13321226a54ceff5ef968b010371807d
leehendrick <imhendrick16@gmail.com>
2024-06-10 07:55:31 +0100

    feature: add get request to bi datas

--- nuxt-app/services/auth.ts
+++ nuxt-app/services/auth.ts
@@ -3,7 +3,7 @@ import { jwtDecode } from 'jwt-decode'
 interface DecodedToken {
   id: number;
   email: string;
-
+  nome: string;
 }
 
 export function login(email: string, senha: string) {
@@ -13,17 +13,27 @@ export function login(email: string, senha: string) {
   });
 }
 
+export function getDataByBi(bi: string){
+  return $fetch(`https://bi.minjusdh.gov.ao/api/identityLostService/identitycardlost/queryRegisterInfo/${bi}`, {
+    method: 'GET'
+  });
+}
 export function decodedToken(token: string): DecodedToken{
   return jwtDecode<DecodedToken>(token);
 }
 
 export function getToken(): string | null {
-  return localStorage.getItem('accessToken');
+  if (typeof window !== 'undefined'){
+    return localStorage.getItem('accessToken');
+  }
+  return null;
 }
 
+// @ts-ignore
 export function getUserData(): DecodedToken | null {
   const token = getToken();
   if (token) {
     return decodedToken(token);
   }
+  return null;
 }
